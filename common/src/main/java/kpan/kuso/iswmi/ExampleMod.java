723c145a2e21a55635196d14abac6328ce9703b4
KatatsumuriPan <katatsumuri.pan@gmail.com>
2024-12-22 22:22:21 +0900

    Initial commit

---
+++ common/src/main/java/kpan/kuso/iswmi/ExampleMod.java
@@ -0,0 +1,9 @@
+package kpan.kuso.iswmi;
+
+public final class ExampleMod {
+    public static final String MOD_ID = "im_struggling_with_my_inventory";
+
+    public static void init() {
+        // Write common init code here.
+    }
+}
